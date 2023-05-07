fb6b80a4ec2702a90e36393651f5b82962f3b3cb
rvot4enota <94300555+rvot4enota@users.noreply.github.com>
2023-05-07 17:11:48 +0400

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int fib(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n = 10;
+    cout << fib(n) << endl;
+    return 0;
+}
